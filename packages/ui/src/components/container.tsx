21da1762d81b842f9f0dd6bcecd1597a7c80a54c
Douglas Pimentel <dev.pimentel@gmail.com>
2025-08-07 20:32:10 -0300

    fix: check types

--- packages/ui/src/components/container.tsx
+++ packages/ui/src/components/container.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@woovi-challenge/ui/lib/utils";
 
 export type ContainerProps = React.ComponentProps<"div">;
 
