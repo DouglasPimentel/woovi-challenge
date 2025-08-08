d92ac458bb93a912c8523d21d3c77ce8a407b1e0
Douglas Pimentel <dev.pimentel@gmail.com>
2025-08-08 00:09:04 -0300

    fix: UserByIDQuery

--- apps/server/src/modules/user/queries/user-by-id.ts
+++ apps/server/src/modules/user/queries/user-by-id.ts
@@ -1,18 +1,19 @@
 import { GraphQLFieldConfig, GraphQLNonNull, GraphQLID } from "graphql";
 import { GraphQLUser } from "@/modules/user/user-type";
-import { UserModel } from "@/modules/user/model";
+import { UserModel, IUser } from "@/modules/user/model";
+import type { GraphQLContext } from "@/schema/context";
 
 type Args = {
   id: string;
 };
 
-export const UserByIDQuery: GraphQLFieldConfig<unknown, unknown, Args> = {
+export const UserByIDQuery: GraphQLFieldConfig<IUser, GraphQLContext, Args> = {
   type: GraphQLUser,
   description: "Fetch a user by their ID",
   args: {
     id: { type: new GraphQLNonNull(GraphQLID) },
   },
-  resolve: async (root, { id }: Args, context) => {
+  resolve: async (root, { id }, context) => {
     const user = await UserModel.findById(id);
 
     return user;
